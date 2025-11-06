1fda53984c07a841a3c0ed7fa6784cdbd5330c69
gracehe04 <gh2313@nyu.edu>
2025-11-05 19:08:19 -0500

    added contact information

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar({ theme, toggleTheme }) {
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#projects">Projects</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><a href="mailto:gh2313@nyu.edu">Contact</a></li>
       </ul>
       <button className="theme-toggle" onClick={toggleTheme}>
         {theme === "light" ? "🌙" : "☀️"}
